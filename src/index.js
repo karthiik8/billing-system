d66c3f97288cb4353e69a5f66ab76d100fc73e7d
Trev241 <trevisliu@gmail.com>
2022-09-06 21:40:03 +0530

    can now add items to invoice

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ class App extends React.Component {
             <div>
                 <h1>Billing System</h1>
                 <div>
-                    <div className='grid-child-element'>
-                        <List />
+                    <div className='grid-upper-workspace'>
+                        <Invoice />
                         <Summary />
                     </div>
                     <div className='grid-control-panel-child-element'>
@@ -21,26 +21,104 @@ class App extends React.Component {
     }
 }
 
-class List extends React.Component {
-    items = [
-        {
-            id: '123456789',
-            name: 'POTATOES', 
-            price: 60,
-            qty: 1
-        },
-        {
-            id: '987456321',
-            name: 'ONIONS',
-            price: 100,
-            qty: 2
-        }
-    ]
+// TODO: Refactor each Component into its own file
 
+class Invoice extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            items: []
+        }
+
+        // This line is important but I do not know what it does
+        // Refer https://stackoverflow.com/questions/2236747/what-is-the-use-of-the-javascript-bind-method
+        // and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Function/bind
+        // This line is necessary to bind the this keyword of updateList to this class. Without it, the function will 
+        // not be able to access this class' state
+        this.updateList = this.updateList.bind(this)
+    }
+
+    updateList(item) {
+        this.setState(prevState => ({
+            items: [...prevState.items, item]
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <NewItem updateList={this.updateList} />
+                <List items={this.state.items} />
+            </div>
+        )
+    }
+}
+
+class NewItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            id: '',
+            name: '',
+            qty: '',
+        }
+
+        this.handleIdChange = this.handleIdChange.bind(this)
+        this.handleNameChange = this.handleNameChange.bind(this)
+        this.handleQtyChange = this.handleQtyChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleIdChange(e) {
+        this.setState({id: e.target.value})
+    }
+
+    handleNameChange(e) {
+        this.setState({name: e.target.value})
+    }
+
+    handleQtyChange(e) {
+        this.setState({qty: e.target.value})
+    }
+
+    handleSubmit(e) {
+        this.props.updateList({
+            id: this.state.id,
+            name: this.state.name,
+            price: 50,  // TODO: Must remove this placeholder price value
+            qty: this.state.qty
+        })
+
+        // Reset state for the next item to be added
+        this.setState({
+            id: '',
+            name: '',
+            qty: ''
+        })
+    }
+ 
+    render() {
+        return (
+            <div>
+                <h2>{this.state.name === '' ? 'Add Item' : this.state.name}</h2>
+                <div>
+                    <input type='text' placeholder='Product ID' value={this.state.id} onChange={this.handleIdChange}/>
+                    <input type='text' placeholder='Product Name' value={this.state.name} onChange={this.handleNameChange}/>
+                    <input type='text' placeholder='Quantity purchased' value={this.state.qty} onChange={this.handleQtyChange}/>
+                    <input type='submit' value='Done' onClick={this.handleSubmit}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+class List extends React.Component {
     render () {
         const rows = []
-        for (let index = 0; index < this.items.length; index++) { 
-            const item = this.items[index];
+        for (let index = 0; index < this.props.items.length; index++) { 
+            const item = this.props.items[index];
             rows.push(
                 <Item id={item.id} name={item.name} price={item.price} qty={item.qty} total={item.price * item.qty} />
             )
@@ -48,6 +126,7 @@ class List extends React.Component {
 
         return (
             <div>
+                <h2>Invoice</h2>
                 <table className='table'>
                     <tr>
                         <th>ID</th>
@@ -81,9 +160,9 @@ class Summary extends React.Component {
     render() {
         return (
             <div>
-                <h1>Summary</h1>
-                <h2>Customer Details</h2>
-                <h2>Transaction Details</h2>
+                <h2>Summary</h2>
+                <h3>Customer Details</h3>
+                <h3>Transaction Details</h3>
             </div>
         )
     }
