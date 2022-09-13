6bdcbb5b7556acd580d39abb8d7db9aa8c18ed26
Trev241 <trevisliu@gmail.com>
2022-09-14 00:58:47 +0530

    list items now have visual cues when selected

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,10 @@
 import React from "react";
 
+import "./list.css"
+
 class List extends React.Component {
+    selectedIndex = null;
+
     constructor(props) {
         super(props)
 
@@ -13,8 +17,8 @@ class List extends React.Component {
     }
 
     handleSelected(e, item, i) {
-        // console.log(this.props.setSelectedItem)
-        this.props.setSelectedItem(item, i)
+        this.selectedIndex = i;
+        this.props.setSelectedItem(item, this.selectedIndex)
     }
 
     render () {
@@ -28,7 +32,7 @@ class List extends React.Component {
 
             rows.push(
                 // TODO: Change key for tr since more than one item can contain the same ID
-                <tr tabIndex="0" onClick={(e) => this.handleSelected(e, item, i)} key={item.id} onFocus={(e) => this.handleSelected(e, item, i)}>
+                <tr className={this.selectedIndex === i ? "selected" : ""} onClick={(e) => this.handleSelected(e, item, i)} key={item.id}>
                     {Object.keys(item).map( 
                         prop => <td key={prop}>{item[prop]}</td>
                     )}
@@ -39,26 +43,28 @@ class List extends React.Component {
 
         if (!empty) 
             rows.push(
-                <tr className="last-row" key="last-row">
-                    <td colSpan="4"><b>FINAL TOTAL</b></td>
+                <tr key="last-row">
+                    <td colSpan="4">FINAL TOTAL</td>
                     <td>{total}</td>
                 </tr>
             )
 
         return (
             <div>
-                <table className="list-table">
-                    <tbody>
+                <table>
+                    <thead>
                         <tr>
                             {/* {Object.keys(this.props.items[0]).map(
                                 prop => <th>{prop.toUpperCase()}</th>
                             )} */}
-                            <th>ID</th>
-                            <th>NAME</th>
-                            <th>PRICE</th>
-                            <th>QUANTITY</th>
-                            <th>SUBTOTAL</th>
+                            <th className="id">ID</th>
+                            <th className="name">NAME</th>
+                            <th className="price">PRICE</th>
+                            <th className="qty">QUANTITY</th>
+                            <th className="subtotal">SUBTOTAL</th>
                         </tr>
+                    </thead>
+                    <tbody>
                         {rows}
                     </tbody>
                 </table>
